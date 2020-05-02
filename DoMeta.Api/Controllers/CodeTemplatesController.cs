0cfe5dc5900bcf390ff1683c17a7e90a93eb57f4
Pieter Deparcq <Pieter@hellocustomer.com>
2020-05-02 13:44:47 +0200

    Finished controller

--- DoMeta.Api/Controllers/CodeTemplatesController.cs
+++ DoMeta.Api/Controllers/CodeTemplatesController.cs
@@ -1,10 +1,12 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using DoMeta.Api.Models.Command;
 using DoMeta.Api.Models.Query;
 using DoMeta.Application.CodeGen.Commands;
 using DoMeta.Application.CodeGen.Queries;
+using DoMeta.Domain.CodeGen;
 using Kledex;
 using Microsoft.AspNetCore.Mvc;
 
@@ -25,10 +27,30 @@ namespace DoMeta.Api.Controllers
         [Route("")]
         public async Task Create([FromRoute] string type, [FromBody] CreateCodeTemplateModel model)
         {
-            await _dispatcher.SendAsync(new CreateCodeTemplate()
+            var template = await _dispatcher.SendAsync<CodeTemplate>(new CreateCodeTemplate()
             {
                 Name = model.Name,
                 SourceType = type,
+            });
+
+            if (model.Value != null)
+            {
+                await _dispatcher.SendAsync(new UpdateCodeTemplate()
+                {
+                    AggregateRootId = template.Id,
+                    Value = model.Value
+                });
+            }
+        }
+
+        [HttpPut]
+        [Route("{templateId}")]
+        public async Task Update([FromRoute] string type, [FromRoute] Guid templateId, [FromBody] UpdateCodeTemplateModel model)
+        {
+            await _dispatcher.SendAsync(new UpdateCodeTemplate()
+            {
+                AggregateRootId = templateId,
+                Value = model.Value
             });
         }
 
---
+++ DoMeta.Api/Models/Command/UpdateCodeTemplateModel.cs
@@ -0,0 +1,7 @@
+﻿namespace DoMeta.Api.Models.Command
+{
+    public class UpdateCodeTemplateModel
+    {
+        public string Value { get; set; }
+    }
+}
